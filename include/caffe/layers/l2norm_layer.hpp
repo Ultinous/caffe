594a4a294af7621147323d19794443978f62e562
Jozsef Nemeth <jnemeth@ultinous.com>
2016-03-29 12:16:39 +0200

    l2norm_layer GPU implementation refs#CGU-259

--- include/caffe/layers/l2norm_layer.hpp
+++ include/caffe/layers/l2norm_layer.hpp
@@ -20,7 +20,7 @@ public:
     : Layer<Dtype>(param) {}
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
                        const vector<Blob<Dtype>*>& top);
-  
+
   virtual inline const char* type() const
   {
     return "L2Norm";
@@ -38,12 +38,12 @@ public:
 protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
                            const vector<Blob<Dtype>*>& top);
-  /*virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-                           const vector<Blob<Dtype>*>& top);*/
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+                           const vector<Blob<Dtype>*>& top);
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
                             const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  /*virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-                            const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);*/
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+                            const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
 
   Blob<Dtype> sum_multiplier_, norm_, squared_, temp_dot_;
