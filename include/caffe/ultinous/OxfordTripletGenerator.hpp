37870ab92681412b688462423543b5d031184a59
Jozsef Nemeth <jnemeth@ultinous.com>
2016-06-02 13:54:18 +0200

    OxfordTripletGenerator: More trials to find hard negatives CGU-259

--- include/caffe/ultinous/OxfordTripletGenerator.hpp
+++ include/caffe/ultinous/OxfordTripletGenerator.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <deque>
 #include <limits>
+#include <list>
 #include <boost/graph/graph_concepts.hpp>
 #include <caffe/ultinous/ImageClassificationModel.h>
 #include <caffe/ultinous/FeatureMap.hpp>
@@ -22,6 +23,7 @@ public:
     : m_modelShuffler( basicModel )
     , m_margin(otp.margin())
     , m_featureMap(FeatureMapContainer<Dtype>::instance(otp.featuremapid()))
+    , m_featureLength(otp.featurelength() )
     , m_sampledPositivePairs( otp.sampledpositivepairs() )
     , m_sampledNegatives( otp.samplednegatives() )
     , m_indexMatrix( m_sampledPositivePairs * (m_sampledNegatives+2) )
@@ -40,6 +42,9 @@ public:
 
     m_featureMap.resize( m_numImagesInModel );
     FeatureCollectorTripletGenerator<Dtype>::init( basicModel );
+
+    m_syncedFeatures.reset( new SyncedMemory( m_sampledPositivePairs*(m_sampledNegatives+2) * m_featureLength * sizeof(Dtype) ) );
+    m_syncedDistances.reset( new SyncedMemory( m_sampledPositivePairs*(m_sampledNegatives+1) * sizeof(Dtype) ) );
   }
 private:
   typedef size_t ImageIndex;
@@ -47,6 +52,22 @@ private:
   typedef size_t SampleIndex;
   typedef ImageClassificationModel::ImageIndexes ImageIndices;
   typedef typename FeatureMap<Dtype>::FeatureVec FeatureVec;
+
+  struct PositivePair {
+    ClassIndex m_classIndex;
+    ImageIndex m_anchor;
+    ImageIndex m_positive;
+    size_t m_lives;
+
+    PositivePair( ClassIndex classIndex, ImageIndex anchor, ImageIndex positive )
+      : m_classIndex(classIndex)
+      , m_anchor(anchor)
+      , m_positive(positive)
+      , m_lives(m_positivePairLives)
+    { }
+  };
+
+  typedef std::list<PositivePair> PositivePairList;
 public:
 
   Triplet nextTriplet()
@@ -62,31 +83,39 @@ public:
       featuresCollected = true;
     }
 
-
     if( m_prefetch.size() == 0 )
       prefetch();
 
-    Triplet t = m_prefetch.front();
-    m_prefetch.pop_front();
+    if ( m_prefetch.size() > 0 )
+    {
+      Triplet t = m_prefetch.front();
+      m_prefetch.pop_front();
+      return t;
+    }
 
-    return t;
+    std::cout << "OxfordTripletGenerator: No hardtriplet found!" << std::endl;
+    return FeatureCollectorTripletGenerator<Dtype>::getInstance().nextTriplet();
   }
 
-  void prefetch()
+private:
+  void prefetch( )
+  {
+    for(size_t i = 0; i < m_prefetchTrials && m_prefetch.size() == 0; ++i )
+      doPrefetch();
+  }
+
+  void doPrefetch()
   {
     //auto start = std::chrono::high_resolution_clock::now();
     //uint64_t sumTime = 0;
+
+    refreshPositivePairList( );
 
     size_t N = m_sampledPositivePairs;
     size_t M = m_sampledNegatives;
 
     size_t featureLength = m_featureMap.getFeatureVec( 0 ).size();
     size_t featureBytes = featureLength*sizeof(Dtype);
-
-    if( !m_syncedFeatures )
-      m_syncedFeatures.reset( new SyncedMemory( m_sampledPositivePairs*(m_sampledNegatives+2) * featureLength * sizeof(Dtype) ) );
-    if( !m_syncedDistances )
-      m_syncedDistances.reset( new SyncedMemory( m_sampledPositivePairs*(m_sampledNegatives+1) * sizeof(Dtype) ) );
 
     Dtype * featureMatrix = (Dtype *)m_syncedFeatures->mutable_cpu_data();
 
@@ -95,11 +124,15 @@ public:
     ClassIndex posClass, negClass;
     ImageIndex anchor, positive, negative;
 
-    for( size_t i = 0; i < N; ++i )
+    typename OxfordTripletGenerator<Dtype>::PositivePairList::iterator ppIt=m_positivePairList.begin();
+    for( size_t i = 0; i < N; ++i, ++ppIt )
     {
       BasicModel const &shuffledModel = m_modelShuffler.shuffledModel();
 
-      nextPositivePair( posClass, anchor, positive );
+      posClass = ppIt->m_classIndex;
+      anchor = ppIt->m_anchor;
+      positive = ppIt->m_positive;
+      --ppIt->m_lives;
 
       *indexMatrixIt++ = anchor;
       FeatureVec const &anchorVec = m_featureMap.getFeatureVec( anchor );
@@ -130,6 +163,8 @@ public:
 
     Triplet t(3);
     size_t j;
+
+    ppIt = m_positivePairList.begin();
     for( size_t i = 0; i < N; ++i )
     {
       for( j = 0; j < M; j++ )
@@ -144,16 +179,32 @@ public:
           break;
         }
       }
-      if( j == M )
-        t = FeatureCollectorTripletGenerator<Dtype>::getInstance().nextTriplet();
 
-      m_prefetch.push_back(t);
+      if( j != M )
+        m_prefetch.push_back(t);
+
+      if( j != M || ppIt->m_lives==0 )
+        ppIt = m_positivePairList.erase(ppIt);
+      else
+        ++ppIt;
     }
     //sumTime+=std::chrono::duration_cast < std::chrono::nanoseconds > (std::chrono::high_resolution_clock::now() - start).count();
     //std::cout << (sumTime / 1000) << std::endl;
   }
 
-private:
+  void refreshPositivePairList( )
+  {
+    ClassIndex posClass;
+    ImageIndex anchor, positive;
+
+    while( m_positivePairList.size() < m_sampledPositivePairs )
+    {
+      nextPositivePair( posClass, anchor, positive );
+
+      m_positivePairList.push_back( PositivePair( posClass, anchor, positive ) );
+    }
+  }
+
   void reset()
   {
     m_modelShuffler.shuffleModel();
@@ -279,6 +330,7 @@ private:
   ImageClassificationModelShuffle m_modelShuffler;
   Dtype m_margin;
   FeatureMap<Dtype>& m_featureMap;
+  size_t m_featureLength;
   size_t m_sampledPositivePairs;
   size_t m_sampledNegatives;
 
@@ -295,8 +347,13 @@ private:
   vector<size_t> m_remainingPairsInClasses;
   uint64_t m_totalRemainingPairs;
 
+  PositivePairList m_positivePairList;
+
   uint32_t xorshf96_x, xorshf96_y, xorshf96_z;
   uint32_t xorshf96_t;
+
+  static const size_t m_prefetchTrials = 50;
+  static const size_t m_positivePairLives = 20;
 };
 
 } // namespace ultinous
