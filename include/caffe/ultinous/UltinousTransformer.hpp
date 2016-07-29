00d34b2091b3ae45ce5625290d841324db78260c
Jozsef Nemeth <jnemeth@ultinous.com>
2016-07-29 09:43:38 +0200

    UltinousTransformer: random Occlusion refs#CGU-259

--- include/caffe/ultinous/UltinousTransformer.hpp
+++ include/caffe/ultinous/UltinousTransformer.hpp
@@ -21,6 +21,130 @@ class UltinousTransformer
 private:
   typedef boost::mt19937 RNGType;
 
+public:
+  UltinousTransformer( UltinousTransformationParameter const& params, Phase phase )
+    : m_params( params )
+    , m_phase( phase )
+  {
+    CHECK_LT(m_params.uniformnoisestrength(), 128)
+      << "uniformNoiseStrength must not exceed 127!";
+
+    CHECK_LE(m_params.minocclusionradius(), m_params.maxocclusionradius())
+      << "minOcclusionRadius must be less than maxOcclusionRadius!";
+
+    m_uniformNoiseStrength = static_cast<int16_t>( m_params.uniformnoisestrength() );
+
+    xorshf96_x=123456789;
+    xorshf96_y=362436069;
+    xorshf96_z=521288629;
+  }
+
+  void transform( cv::Mat& cv_img )
+  {
+    if( m_phase != TRAIN ) return;
+
+    // Apply color transformation
+    if( m_params.luminanceminscale() != 1.0f
+      || m_params.luminancemaxscale() != 1.0f
+      || m_params.saturationminscale() != 1.0f
+      || m_params.saturationmaxscale() != 1.0f )
+    {
+      float lumCoef = m_params.luminanceminscale()
+        + (m_params.luminancemaxscale()-m_params.luminanceminscale())
+          *static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+      float satCoef = m_params.saturationminscale()
+        + (m_params.saturationmaxscale()-m_params.saturationminscale())
+          *static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+      cv::cvtColor( cv_img, cv_img, CV_BGR2HLS );
+
+      for( size_t x = 0; x < cv_img.rows; ++x )
+      {
+        for( size_t y = 0; y < cv_img.cols; ++y )
+        {
+          cv::Vec3b &pixel = cv_img.at<cv::Vec3b>( x, y );
+
+          pixel[1] = static_cast<uint8_t>( std::min(255.0f, float(pixel[1])*lumCoef) );
+          pixel[2] = static_cast<uint8_t>( std::min(255.0f, float(pixel[2])*satCoef) );
+
+          cv_img.at<cv::Vec3b>( x, y ) = pixel;
+        }
+      }
+      cv::cvtColor( cv_img, cv_img, CV_HLS2BGR );
+    }
+
+    // Apply affine transformation
+    cv::Mat cv_affine;
+    applyAffine( cv_img, cv_affine );
+    cv_img = cv_affine;
+
+    // Occlusion
+    if( m_params.numocclusions() != 0 && m_params.maxocclusionradius() != 0 )
+    {
+
+      for( size_t i = 0; i < m_params.numocclusions(); ++ i )
+      {
+        size_t r = m_params.minocclusionradius()
+        + (m_params.maxocclusionradius()-m_params.minocclusionradius())
+          *static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+        size_t cx = r + std::rand() % (cv_img.rows - 2*r);
+        size_t cy = r + std::rand() % (cv_img.cols - 2*r);
+
+        cv::circle( cv_img, cv::Point(cx,cy), r, cv::Scalar(0), -1 );
+      }
+    }
+
+    // Uniform noise
+    if( m_uniformNoiseStrength != 0 )
+    {
+      CHECK(cv_img.depth() == CV_8U) << "Image data type must be unsigned byte";
+      CHECK( cv_img.isContinuous() ) << "OpenCV image matrix must be contiuously stored in memory!";
+
+      //boost::uniform_int<int16_t> distribution( -m_uniformNoiseStrength, m_uniformNoiseStrength );
+      //boost::variate_generator< RNGType, boost::uniform_int<int16_t> > noiseGenerator(m_rng, distribution);
+
+      size_t N = cv_img.channels() * cv_img.rows * cv_img.cols;
+
+      //vector<int16_t> rndVec(N);
+      //generate(rndVec.begin(), rndVec.end(), noiseGenerator);
+
+
+      uint8_t * p = cv_img.data;
+      int16_t MIN = static_cast<int16_t>(0);
+      int16_t MAX = static_cast<int16_t>(std::numeric_limits<uint8_t>::max());
+      for( size_t i =0; i < N; ++i, ++p) {
+        *p = static_cast<uint8_t>(
+          std::max( MIN, std::min( MAX,
+            static_cast<int16_t>(static_cast<int16_t>(*p)
+              + static_cast<int16_t>(myRandom(1+2*m_uniformNoiseStrength))-m_uniformNoiseStrength )
+          ) )
+        );
+      }
+    }
+
+    // Padding
+    if( m_params.padtop() != 0 || m_params.padbottom() != 0
+      || m_params.padleft() != 0 || m_params.padright() != 0 )
+    {
+      cv::Mat dst;
+
+      cv::copyMakeBorder( cv_img, dst, m_params.padtop(), m_params.padbottom()
+                    , m_params.padleft(), m_params.padright(), cv::BORDER_CONSTANT );
+
+      cv_img = dst;
+    }
+
+
+    /*static int ccc = 0;
+    std::string fname = boost::lexical_cast<std::string>(ccc) + ".png";
+    cv::imwrite( fname, cv_img );
+    ++ccc;*/
+
+  }
+
+private:
   void applyAffine( cv::Mat& src, cv::Mat& dst )
   {
     const double pi = std::acos(-1);
@@ -86,98 +210,27 @@ private:
     cv::warpAffine( src, dst, affine, src.size(), CV_INTER_CUBIC );
   }
 
-public:
-  UltinousTransformer( UltinousTransformationParameter const& params, Phase phase )
-    : m_params( params )
-    , m_phase( phase )
-  {
-    CHECK_LT(m_params.uniformnoisestrength(), 128)
-      << "uniformNoiseStrength must not exceed 127!";
-
-    m_uniformNoiseStrength = static_cast<int16_t>( m_params.uniformnoisestrength() );
-
+  uint32_t xorshf96(void) {          //period 2^96-1
+    xorshf96_x ^= xorshf96_x << 16;
+    xorshf96_x ^= xorshf96_x >> 5;
+    xorshf96_x ^= xorshf96_x << 1;
+    xorshf96_t = xorshf96_x;
+    xorshf96_x = xorshf96_y;
+    xorshf96_y = xorshf96_z;
+    xorshf96_z = xorshf96_t ^ xorshf96_x ^ xorshf96_y;
+    return xorshf96_z;
   }
 
-  void transform( cv::Mat& cv_img )
+  uint64_t myRandom( uint64_t N )
   {
-    if( m_phase != TRAIN ) return;
-
-    // Apply color transformation
-    if( m_params.luminanceminscale() != 1.0f
-      || m_params.luminancemaxscale() != 1.0f
-      || m_params.saturationminscale() != 1.0f
-      || m_params.saturationmaxscale() != 1.0f )
+    if( N-1 <= std::numeric_limits<uint32_t>::max() )
     {
-      float lumCoef = m_params.luminanceminscale()
-        + (m_params.luminancemaxscale()-m_params.luminanceminscale())
-          *static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-
-      float satCoef = m_params.saturationminscale()
-        + (m_params.saturationmaxscale()-m_params.saturationminscale())
-          *static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-
-      cv::cvtColor( cv_img, cv_img, CV_BGR2HLS );
-
-      for( size_t x = 0; x < cv_img.rows; ++x )
-      {
-        for( size_t y = 0; y < cv_img.cols; ++y )
-        {
-          cv::Vec3b &pixel = cv_img.at<cv::Vec3b>( x, y );
-
-          pixel[1] = static_cast<uint8_t>( std::min(255.0f, float(pixel[1])*lumCoef) );
-          pixel[2] = static_cast<uint8_t>( std::min(255.0f, float(pixel[2])*satCoef) );
-
-          cv_img.at<cv::Vec3b>( x, y ) = pixel;
-        }
-      }
-      cv::cvtColor( cv_img, cv_img, CV_HLS2BGR );
+      return uint64_t(xorshf96()) % N;
     }
-
-    // Apply affine transformation
-    cv::Mat cv_affine;
-    applyAffine( cv_img, cv_affine );
-    cv_img = cv_affine;
-
-    // Uniform noise
-    if( m_uniformNoiseStrength != 0 )
-    {
-      CHECK(cv_img.depth() == CV_8U) << "Image data type must be unsigned byte";
-      CHECK( cv_img.isContinuous() ) << "OpenCV image matrix must be contiuously stored in memory!";
-
-      boost::uniform_int<int16_t> distribution( -m_uniformNoiseStrength, m_uniformNoiseStrength );
-      boost::variate_generator< RNGType, boost::uniform_int<int16_t> > noiseGenerator(m_rng, distribution);
-
-      size_t N = cv_img.channels() * cv_img.rows * cv_img.cols;
-
-      uint8_t * p = cv_img.data;
-      int16_t MIN = static_cast<int16_t>(0);
-      int16_t MAX = static_cast<int16_t>(std::numeric_limits<uint8_t>::max());
-      for( size_t i =0; i < N; ++i, ++p)
-        *p = static_cast<uint8_t>(
-          std::max( MIN, std::min( MAX,
-            static_cast<int16_t>(static_cast<int16_t>(*p) + static_cast<int16_t>(noiseGenerator()))
-          ) )
-        );
-    }
-
-    // Padding
-    if( m_params.padtop() != 0 || m_params.padbottom() != 0
-      || m_params.padleft() != 0 || m_params.padright() != 0 )
-    {
-      cv::Mat dst;
-
-      cv::copyMakeBorder( cv_img, dst, m_params.padtop(), m_params.padbottom()
-                    , m_params.padleft(), m_params.padright(), cv::BORDER_CONSTANT );
-
-      cv_img = dst;
-    }
-
-    /*
-    static int ccc = 0;
-    std::string fname = boost::lexical_cast<std::string>(ccc) + ".png";
-    cv::imwrite( fname, cv_img );
-    ++ccc;
-    */
+    uint64_t r = 0;
+    r += uint64_t(xorshf96());
+    r += uint64_t(xorshf96())<<32;
+    return r % N;
   }
 private:
   UltinousTransformationParameter const& m_params;
@@ -185,6 +238,11 @@ private:
 
   int16_t m_uniformNoiseStrength;
   RNGType m_rng;
+
+    /* Random generator variables */
+  uint32_t xorshf96_x, xorshf96_y, xorshf96_z;
+  uint32_t xorshf96_t;
+
 };
 
 }  // namespace ultinous
